48cd01e5dec57fccd8f7657f4476ab4b7f78bc6c
vlad.luta <vlad.luta@vodafone.com>
2024-04-08 10:15:55 +0300

    initial commit

---
+++ src/test/java/springframework/simplilearnecommerce/SimplilearnECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.simplilearnecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplilearnECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
